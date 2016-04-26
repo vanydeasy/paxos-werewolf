c3770138a823d6bcf7baa4b8b99736b17bc9031b
Venny Larasati <venny_la@yahoo.com>
2016-04-26 18:38:53 +0700

    add listener in client (not working yet)

--- werewolf/src/client/Client.java
+++ werewolf/src/client/Client.java
@@ -22,12 +22,14 @@ import java.util.logging.Logger;
 import org.json.simple.JSONArray;
 import server.Server;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
  * @author vanyadeasy
  */
-public class Client {
+public class Client extends Thread {
     // FOR TCP CONNECTION
     public String SERVER_HOSTNAME;
     public int COMM_PORT;  // socket port for client comms
@@ -41,6 +43,7 @@ public class Client {
     private int player_id;
     private String role;
     private JSONArray players = new JSONArray();
+    private boolean proposer;
     
     public Client() {
         SERVER_HOSTNAME = "127.0.1.1";
@@ -65,6 +68,8 @@ public class Client {
             if(client.player_id == (Integer)((JSONObject)client.players.get(client.players.size()-2)).get("player_id")
                 || client.player_id == (Integer)((JSONObject)client.players.get(client.players.size()-1)).get("player_id")) {
                 // PROPOSER pid dua terbesar (player ke n dan n­1) 
+                client.proposer = true;
+                
                 try {
                     System.out.println("You can propose");
                     
@@ -83,10 +88,7 @@ public class Client {
                         unreliableSender.send(sendPacket, 1.00);
                     }
                     
-                    // 
-                    for(int i=0;i<client.players.size();i++) {
-                        
-                    }
+                    
                 } catch (SocketException ex) {
                     Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (UnknownHostException ex) {
@@ -97,11 +99,9 @@ public class Client {
                 
             } else {
                 // ACCEPTOR
+                client.proposer = false;
                 System.out.println("You cannot propose");
-                // PAXOS PREPARE PROPOSAL
-                for(int i=0;i<2;i++) {
-                    String received = client.listenToUDP();
-                }
+
             }
         }
         
@@ -112,10 +112,10 @@ public class Client {
         System.out.println("Connecting to server...");
         try {
             this.socket = new Socket(SERVER_HOSTNAME, COMM_PORT);
+            System.out.println("Connected!");
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println("Connected!");
     }
     
     public Object listenToServer() {
@@ -221,6 +221,64 @@ public class Client {
                 System.out.println(recv.toJSONString());
             }
         } while(!recv.get("status").equals("ok"));
+    }
+    
+    @Override
+    public void run() {
+        do {
+            System.out.println ("New Communication Thread Started");
+            String recv = listenToUDP();
+            
+            JSONParser parser = new JSONParser();
+            JSONObject jsonRecv = new JSONObject();
+            try {
+                jsonRecv = (JSONObject) parser.parse(recv);
+            } catch (ParseException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            JSONObject temp = new JSONObject();
+            if (proposer == false){ // acceptor
+                if(jsonRecv.get("method").equals("prepare_proposal")){
+                    temp.put("status", "ok");
+                    temp.put("description", "accepted");
+                    temp.put("previous_accepted", ""); // gue ga ngerti previous kpu_id maksudnya apa
+                } else if (jsonRecv.get("method").equals("accept_proposal")){
+                    // jika ini proposer yang pertama (?) karena leader cuma boleh satu
+                        temp.put("status", "ok");
+                        temp.put("description", "accepted");
+                    // jika bukan
+                        temp.put("status", "fail");
+                        temp.put("description", "rejected");
+                }
+            } else { // proposer
+                
+            }
+            
+            DatagramSocket datagramSocket;
+            UnreliableSender unreliableSender;
+            try {
+                datagramSocket = new DatagramSocket();
+                unreliableSender = new UnreliableSender(datagramSocket);
+                byte[] sendData = temp.toJSONString().getBytes();
+                
+                String ipAddress = ""; // dapet darimana?
+                int port = 0; // dapet darimana?
+                        
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(ipAddress.substring(1)), port);
+                unreliableSender.send(sendPacket, 1.00);
+                
+            } catch (SocketException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
+            
+
+        } while (true);
+        
     }
     
 }
